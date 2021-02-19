f64166cf94e90d36bfd539758645a67d091bc2ba
Rainer Gevers <rainergeve@gmail.com>
2021-02-19 22:18:58 +0200

    Working with objects, properties and methods

--- 2. Javascript refresher/play.js
+++ 2. Javascript refresher/play.js
@@ -1,27 +1,38 @@
-const name = "Rainer";
-let age = 28;
-const hasHobbies = true;
+// const name = "Rainer";
+// let age = 28;
+// const hasHobbies = true;
 
-age = 29;
+// age = 29;
 
-const summarizeUser = (userName, userAge, userHasHobbies) => {
+// const summarizeUser = (userName, userAge, userHasHobbies) => {
 
-    return ( 'Name is ' +
-     userName + 
-     ', age is ' + 
-     userAge + 
-     ' and the user has hobbies: ' + 
-     userHasHobbies 
-     );
+//     return ( 'Name is ' +
+//      userName + 
+//      ', age is ' + 
+//      userAge + 
+//      ' and the user has hobbies: ' + 
+//      userHasHobbies 
+//      );
 
-}
+// }
 
-// const add = (a,b) => a + b;
-//const addOne = a => a + 1; // hakkies is optioneel as daar net 1 input is
+// // const add = (a,b) => a + b;
+// //const addOne = a => a + 1; // hakkies is optioneel as daar net 1 input is
 
-const addRandom = () => 1 + 2;
+// const addRandom = () => 1 + 2;
 
-// console.log(add(1,2));
-// console.log(addOne(1));
-console.log(addRandom());
-console.log(summarizeUser(name, age, hasHobbies));
+// // console.log(add(1,2));
+// // console.log(addOne(1));
+// console.log(addRandom());
+// console.log(summarizeUser(name, age, hasHobbies));
+
+const person = {
+    name: 'Rainer',
+    age: 28,
+    greet () {
+        console.log('Hi, I am ' + this.name)
+    }
+};
+
+person.greet();
+console.log(person);
