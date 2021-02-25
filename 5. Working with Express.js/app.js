82d0a2f1f1d142b38fbd58080ec75f03d752c478
Rainer Gevers <rainer@fastcomm.com>
2021-02-25 09:34:18 +0200

    Using Routes

--- 5. Working with Express.js/app.js
+++ 5. Working with Express.js/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
-});
+app.use('/add-product', (req, res, next) => {
+    console.log("In another middleware");
+    res.send('<h1>The "add-product" page</h1>');
+    // ...
+  });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log("In another middleware");
   res.send('<h1>hello from express</h1>');
   // ...
