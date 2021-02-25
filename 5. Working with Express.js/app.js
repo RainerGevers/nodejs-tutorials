542d0f008478eb342ea8e70954645f6f5a1ba1f2
Rainer Gevers <rainer@fastcomm.com>
2021-02-25 09:48:29 +0200

    Limit middleware execution to certain verbs

--- 5. Working with Express.js/app.js
+++ 5. Working with Express.js/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
   res.send(
@@ -12,9 +12,9 @@ app.use("/add-product", (req, res, next) => {
   // ...
 });
 
-app.use("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
