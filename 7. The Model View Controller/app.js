fa95111f035ad11f4692dea79b9f21cfdbd079c3
Rainer Gevers <rainergeve@gmail.com>
2021-03-04 17:43:28 +0200

    init mvc

---
+++ 7. The Model View Controller/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+const path = require("path");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+// app.set('view engine','pug');
+
+// app.engine("handlebars", expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+// app.set("view engine", "handlebars");
+
+app.set('view engine', 'ejs')
+app.set("views", "views"); // Not necessary, already default
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {pageTitle: 'Page not Found'});
+});
+
+app.listen(3000);
---
+++ 7. The Model View Controller/routes/shop.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+
+const rootDir = require("../util/path");
+const router = express.Router();
+
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  res.render("shop", {
+    prods: adminData.products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: adminData.products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+  // ...
+});
+
+module.exports = router;
