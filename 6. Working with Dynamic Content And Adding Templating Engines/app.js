687c0efd7effa9208f15c0f9c6a210bd2550aca3
Rainer Gevers <rainergeve@gmail.com>
2021-03-04 16:55:46 +0200

    starting with handlebars

--- 6. Working with Dynamic Content And Adding Templating Engines/app.js
+++ 6. Working with Dynamic Content And Adding Templating Engines/app.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path')
+const expressHbs = require("express-handlebars");
+const path = require("path");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.set('view engine','pug');
-app.set('views','views'); // Not necessary, already default
+// app.set('view engine','pug');
+
+app.engine("handlebars", expressHbs());
+app.set("view engine", "handlebars");
+app.set("views", "views"); // Not necessary, already default
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404')
+  res.status(404).render("404");
 });
 
 app.listen(3000);
