4520d2c26742a23279251a0700cea3fd425e0b92
Rainer Gevers <rainergeve@gmail.com>
2021-03-06 15:46:31 +0200

    Worked on cart model

--- 9. Dynamic Routes and Advanced Models/routes/shop.js
+++ 9. Dynamic Routes and Advanced Models/routes/shop.js
@@ -6,7 +6,9 @@ const shopController = require("../controllers/shop");
 
 router.get("/", shopController.getProducts);
 router.get("/products", shopController.getIndex);
+router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
+router.post("/cart", shopController.postCart)
 router.get("/checkout", shopController.getCheckout);
 router.get("/orders", shopController.getOrders);
 
