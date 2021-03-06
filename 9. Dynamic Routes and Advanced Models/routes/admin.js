02524bda26242299329f7c8e4ed605393cd3547a
Rainer Gevers <rainergeve@gmail.com>
2021-03-06 17:44:40 +0200

    Wrapping up cart management

--- 9. Dynamic Routes and Advanced Models/routes/admin.js
+++ 9. Dynamic Routes and Advanced Models/routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 const adminController = require("../controllers/admin");
 
-
 router.get("/add-product", adminController.getAddProduct);
 router.post("/add-product", adminController.postAddProduct);
-router.get('/edit-product/:id', adminController.getEditProduct)
-router.post('/edit-product', adminController.postEditProduct)
+router.get("/edit-product/:id", adminController.getEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
 router.get("/products", adminController.getProducts);
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
