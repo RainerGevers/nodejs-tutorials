15a3530aa928bb3e45b8e2909b3ad72f32665bbc
Rainer Gevers <rainer@fastcomm.com>
2021-03-16 15:38:09 +0200

    some more mongodb work

--- 12. Working with NOSQL and using MongoDB/models/product.js
+++ 12. Working with NOSQL and using MongoDB/models/product.js
@@ -1,3 +1,4 @@
+const mongoDb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -10,10 +11,37 @@ class Product {
 
     save() {
         const db = getDb();
-        return db.collection("products")
+        return db
+            .collection("products")
             .insertOne(this)
             .then((result) => {
-                console.log(result);
+                // console.log(result);
+            })
+            .catch((err) => console.log(err));
+    }
+
+    static fetchAll() {
+        const db = getDb();
+        return db
+            .collection("products")
+            .find()
+            .toArray()
+            .then((products) => {
+                console.log(products);
+                return products;
+            })
+            .catch((err) => console.log(err));
+    }
+
+    static findById(prodId) {
+        const db = getDb();
+        return db
+            .collection("products")
+            .find({ _id: new mongoDb.ObjectId(prodId) })
+            .next()
+            .then((product) => {
+                console.log(product);
+                return product;
             })
             .catch((err) => console.log(err));
     }
